c0f2483f5db362dffb2ee384dff07b95a41eb0be
Hafedh Ben Haj Ali <70435711+hafedhBel@users.noreply.github.com>
2023-10-23 14:59:12 +0100

    Add files via upload

--- solution.js
+++ solution.js
@@ -1,18 +1,18 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.post("/submit", (req, res) => {
-  const numLetters = req.body["fName"].length + req.body["lName"].length;
-  res.render("index.ejs", { numberOfLetters: numLetters });
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
 });
 
 app.listen(port, () => {
